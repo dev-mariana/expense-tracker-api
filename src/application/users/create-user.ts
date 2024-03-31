5a0e2ee2fe8bf1e283ab0e0ac076c7d08996bfc1
Mariana Bastos <maribastossrodrigues@gmail.com>
2024-03-31 17:45:44 -0300

    feat: add create user route

---
+++ src/application/users/create-user.ts
@@ -0,0 +1,28 @@
+import { FastifyInstance } from "fastify";
+import { z } from "zod";
+import { prisma } from "../../infra/database/prisma";
+
+export async function createUser(app: FastifyInstance) {
+  app.post("/api/users", async (req, reply) => {
+    const createUserBody = z.object({
+      name: z.string(),
+      age: z.number(),
+      email: z.string().email().min(6),
+      password: z.string().min(6),
+    });
+
+    const { name, age, email, password } = createUserBody.parse(req.body);
+
+    const user = await prisma.user.create({
+      data: {
+        name,
+        age,
+        email,
+        password,
+        created_at: new Date(),
+      },
+    });
+
+    return reply.status(201).send({ user });
+  });
+}
