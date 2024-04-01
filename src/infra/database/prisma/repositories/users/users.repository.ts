8b615c03ab3a0c07806a94d33ccdd58bf6bb24e0
Mariana Bastos <maribastossrodrigues@gmail.com>
2024-04-01 17:39:53 -0300

    refactor: add users repository interface

--- src/infra/database/prisma/repositories/users/users.repository.ts
+++ src/infra/database/prisma/repositories/users/users.repository.ts
@@ -1,11 +1,12 @@
 import { PrismaClient } from "@prisma/client";
-import { CreateUserDTO } from "../../../../application/users/dto/create-user.dto";
-import { UserEntity } from "../../../../application/users/entities/user";
+import { CreateUserDTO } from "../../../../../application/users/dto/create-user.dto";
+import { User } from "../../../../../domain/users/models/user";
+import { IUsersRepository } from "../../../../../domain/users/repositories/users.repository";
 
-export class UsersRepository {
+export class UsersRepository implements IUsersRepository {
   constructor(private prisma: PrismaClient) {}
 
-  async create(createUserDTO: CreateUserDTO): Promise<UserEntity> {
+  async create(createUserDTO: CreateUserDTO): Promise<User> {
     const { name, age, email, password } = createUserDTO;
     const user = await this.prisma.user.create({
       data: {
