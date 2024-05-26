8a0ed73571448cf28d0fab7e569d79168acb3c18
Mariana Bastos <maribastossrodrigues@gmail.com>
2024-05-26 16:03:42 -0300

    feat: add findUsers at UsersController

--- src/application/users/controllers/users.controller.ts
+++ src/application/users/controllers/users.controller.ts
@@ -1,12 +1,12 @@
-import { FastifyInstance } from "fastify";
-import { createUserSchema } from "../dto/create-user.dto";
-import { UsersService } from "../services/users.service";
+import { FastifyInstance } from 'fastify';
+import { createUserSchema } from '../dto/create-user.dto';
+import { UsersService } from '../services/users.service';
 
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   async createUser(app: FastifyInstance) {
-    app.post("/api/users", async (req, reply) => {
+    app.post('/api/users', async (req, reply) => {
       const { name, age, email, password } = createUserSchema.parse(req.body);
 
       const user = await this.usersService.create({
@@ -17,6 +17,14 @@ export class UsersController {
       });
 
       return reply.status(201).send(user);
+    });
+  }
+
+  async findUsers(app: FastifyInstance) {
+    app.get('/api/users', async (req, reply) => {
+      const users = await this.usersService.findAll();
+
+      return reply.status(200).send(users);
     });
   }
 }
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
-import * as dotenv from "dotenv";
-import fastify from "fastify";
-import { UsersController } from "./application/users/controllers/users.controller";
-import { UsersService } from "./application/users/services/users.service";
-import { prisma } from "./infra/database/prisma/prisma";
-import { UsersRepository } from "./infra/database/prisma/repositories/users/users.repository";
+import * as dotenv from 'dotenv';
+import fastify from 'fastify';
+import { UsersController } from './application/users/controllers/users.controller';
+import { UsersService } from './application/users/services/users.service';
+import { prisma } from './infra/database/prisma/prisma';
+import { UsersRepository } from './infra/database/prisma/repositories/users/users.repository';
 
 dotenv.config();
 
@@ -16,7 +16,8 @@ const usersService = new UsersService(usersRepository);
 const usersController = new UsersController(usersService);
 
 usersController.createUser(app);
+usersController.findUsers(app);
 
 app.listen({ port: Number(port) }).then(() => {
-  console.log("Server is running!");
+  console.log('Server is running!');
 });
