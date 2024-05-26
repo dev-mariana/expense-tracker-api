7e172cbd3f9116660ab303552c076982c861498e
Mariana Bastos <maribastossrodrigues@gmail.com>
2024-05-26 16:03:14 -0300

    feat: add findAll at IUsersService and UsersService

--- src/application/users/services/users.service.ts
+++ src/application/users/services/users.service.ts
@@ -1,12 +1,16 @@
-import { User } from "../../../domain/users/models/user";
-import { IUsersService } from "../../../domain/users/services/users.service";
-import { UsersRepository } from "../../../infra/database/prisma/repositories/users/users.repository";
-import { CreateUserDTO } from "../dto/create-user.dto";
+import { User } from '../../../domain/users/models/user';
+import { IUsersService } from '../../../domain/users/services/users.service';
+import { UsersRepository } from '../../../infra/database/prisma/repositories/users/users.repository';
+import { CreateUserDTO } from '../dto/create-user.dto';
 
 export class UsersService implements IUsersService {
   constructor(private readonly usersRepository: UsersRepository) {}
 
   async create(user: CreateUserDTO): Promise<User> {
     return await this.usersRepository.create(user);
+  }
+
+  async findAll(): Promise<User[]> {
+    return await this.usersRepository.findAll();
   }
 }
--- src/domain/users/services/users.service.ts
+++ src/domain/users/services/users.service.ts
@@ -1,5 +1,6 @@
-import { User } from "../models/user";
+import { User } from '../models/user';
 
 export interface IUsersService {
   create(user: User): Promise<User>;
+  findAll(): Promise<User[]>;
 }
